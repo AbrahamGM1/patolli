33e07986527390c7bb7531761d91c3e2f6e2b144
Abraham Sered Gómez Martínez <abraham.gomez228796@potros.itson.edu.mx>
2022-10-07 16:29:43 -0700

    Agregado el boolean enJuego en la entidad ficha

--- src/entidades/Ficha.java
+++ src/entidades/Ficha.java
@@ -2,18 +2,26 @@ package entidades;
 
 /**
  *
- * @author 
- * Luis Gonzalo Cervantes Rivera 00000228549
- * Gabriel Francisco Piñuelas Ramos 00000230626
- * Ricardo Pacheco Urias 00000229178
- * Abraham Sered Gómez Martínez 00000228796
- * 
+ * @author Luis Gonzalo Cervantes Rivera 00000228549 Gabriel Francisco Piñuelas
+ * Ramos 00000230626 Ricardo Pacheco Urias 00000229178 Abraham Sered Gómez
+ * Martínez 00000228796
+ *
  */
 public class Ficha {
 
     private int posicion;
     private int id;
+    private boolean enJuego;
 
+    public Ficha() {
+    }
+    
+    public Ficha(int posicion, int id, boolean enJuego) {
+        this.posicion = posicion;
+        this.id = id;
+        this.enJuego = enJuego;
+    }
+        
     public Ficha(int id, int posicion) {
         this.id = id;
         this.posicion = posicion;
@@ -34,4 +42,14 @@ public class Ficha {
     public void setPosicion(int posicion) {
         this.posicion = posicion;
     }
+
+    public boolean isEnJuego() {
+        return enJuego;
+    }
+
+    public void setEnJuego(boolean enJuego) {
+        this.enJuego = enJuego;
+    }
+
+
 }
