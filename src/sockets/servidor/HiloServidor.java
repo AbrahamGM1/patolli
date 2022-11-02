0f43b403782d466739d6c57ee19bc5f0e36b7df4
LAPTOP-CPPRNHU9\luisg <luisgonzalocervantes@gmail.com>
2022-11-02 12:05:04 -0700

    Creación del socket e hilo del servidor, conexión será en el puerto 2027

---
+++ src/sockets/servidor/HiloServidor.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sockets.servidor;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author luisg
+ */
+public class HiloServidor implements Runnable {
+
+    //Declaramos las variables que utiliza el hilo para estar recibiendo y mandando mensajes
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private boolean conectado = true;
+
+    //id del jugador
+    private int id;
+
+    //Lista de los usuarios conectados al servidor
+    private LinkedList<Socket> usuarios = new LinkedList<>();
+
+    /**
+     * Constructor que recibe el socket que atenderá el hilo y la lista de
+     * jugadores
+     *
+     * @param socket socket que atenderá el hilo
+     * @param usuarios lista de jugadores
+     * @param id id del jugador
+     */
+    public HiloServidor(Socket socket, LinkedList<Socket> usuarios, int id) {
+        this.socket = socket;
+        this.usuarios = usuarios;
+        this.id = id;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            //Inicializamos los canales de comunicación y mandamos el turno a cada jugador
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            String msg = "" + id;
+            out.writeUTF(msg);
+            
+            //Ciclo infinito que estará escuchando por los movimientos de cada jugador
+            while (true) {
+                String recibidos = in.readUTF();
+                
+                for (Socket u : usuarios) {
+                    out = new DataOutputStream(u.getOutputStream());
+                    out.writeUTF(recibidos);
+                }
+            }
+        } catch (Exception e) {
+            
+        }
+    }
+}
---
+++ src/sockets/servidor/Server.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sockets.servidor;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author luisg
+ */
+public class Server {
+
+    //inicialización de puerto
+    private final int puerto = 2027;
+
+    //número máximo de conexiones
+    private final int noConexiones = 4;
+
+    //Lista de sockets para guardar el socket de cada jugador
+    private LinkedList<Socket> usuarios = new LinkedList<>();
+
+    public static int id = 0;
+
+    /**
+     * Fución para que el servidor empiece a recibir conexiones de clientes
+     */
+    public void escuchar() {
+        try {
+            //Creamos el socket servidor
+            ServerSocket servidor = new ServerSocket(puerto, noConexiones);
+
+            //Ciclo infinito para estar escuchando por nuevos jugadores
+            System.out.println("Esperando jugadores...");
+            while (true) {
+                //Cuando un jugador se conecte guardamos el socket en esta línea
+                Socket cliente = servidor.accept();
+
+                //Se agrega el socket a la lista
+                usuarios.add(cliente);
+
+                Runnable run = new HiloServidor(cliente, usuarios, id);
+                Thread hilo = new Thread(run);
+                hilo.start();
+                id++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Método main para correr el servidor
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.escuchar();
+    }
+}
