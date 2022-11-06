dba01088fd78f95e827cde2ada711d816cb17ec2
LAPTOP-CPPRNHU9\luisg <luisgonzalocervantes@gmail.com>
2022-11-05 17:06:59 -0700

    Creación de la clase Client la cual actuará como un Socket

---
+++ src/sockets/jugador/Client.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sockets.jugador;
+
+/**
+ *
+ * @author luisg
+ */
+public class Client {
+    
+    private final int puerto = 2027;
+    
+    //localhost será el valor temporalmente
+    private String host = "localhost";
+}
