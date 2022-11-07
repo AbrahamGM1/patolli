b9535d988cc101acac20f2ff8f2e50b09b96f5af
LAPTOP-CPPRNHU9\luisg <luisgonzalocervantes@gmail.com>
2022-11-06 20:52:01 -0700

    Eliminación de subpaquetes servidor y jugador, paquete sockets renombrado a servidor y clases necesarias para el server movidas a este

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sockets.servidor;
+package server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
