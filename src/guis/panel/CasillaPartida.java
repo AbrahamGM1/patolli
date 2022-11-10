afcab00d378dc8c2679cb43849a5d9109f0483e9
LAPTOP-CPPRNHU9\luisg <luisgonzalocervantes@gmail.com>
2022-11-10 16:44:45 -0700

    Inicialización de los caminos del resto de jugadores en el tablero 10

--- src/guis/panel/CasillaPartida.java
+++ src/guis/panel/CasillaPartida.java
@@ -5,64 +5,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author 
- * Luis Gonzalo Cervantes Rivera 00000228549
- * Gabriel Francisco Piñuelas Ramos 00000230626
- * Ricardo Pacheco Urias 00000229178
- * Abraham Sered Gómez Martínez 00000228796
- * 
+ * @author Luis Gonzalo Cervantes Rivera 00000228549 Gabriel Francisco Piñuelas
+ * Ramos 00000230626 Ricardo Pacheco Urias 00000229178 Abraham Sered Gómez
+ * Martínez 00000228796
+ *
  */
 public class CasillaPartida {
-    List <Casilla> Casilla10j1 = new ArrayList();
-    public List <Casilla> casilla10J2 = new ArrayList<>();
-    List <Casilla> Casilla12 = new ArrayList();
-    List <Casilla> Casilla14 = new ArrayList();
+
+    public List<Casilla> Casilla10j1 = new ArrayList<>();
+    public List<Casilla> casilla10J2 = new ArrayList<>();
+    public List<Casilla> casilla10J3 = new ArrayList<>();
+    public List<Casilla> casilla10J4 = new ArrayList<>();
+    public List<Casilla> Casilla12 = new ArrayList<>();
+    public List<Casilla> Casilla14 = new ArrayList<>();
 
     public CasillaPartida() {
+
     }
-    
-    public List inicializarCasilla10j1(){
-    Casilla10j1.add(new Casilla(0,533, 360));
-    Casilla10j1.add(new Casilla(1,564, 331));
-    Casilla10j1.add(new Casilla(2,597, 298));
-    Casilla10j1.add(new Casilla(3,627, 267));
-    Casilla10j1.add(new Casilla(4,662, 231));
-    Casilla10j1.add(new Casilla(5,629, 200));
-    Casilla10j1.add(new Casilla(6,590, 235));
-    Casilla10j1.add(new Casilla(7,561, 264));
-    Casilla10j1.add(new Casilla(8,529, 297));
-    Casilla10j1.add(new Casilla(9,498, 328));
-    Casilla10j1.add(new Casilla(10,466, 357));
-    
-    Casilla10j1.add(new Casilla(11,434, 329));
-    Casilla10j1.add(new Casilla(12,400, 299));
-    Casilla10j1.add(new Casilla(13,369, 268));
-    Casilla10j1.add(new Casilla(14,337, 238));
-    Casilla10j1.add(new Casilla(15,300, 204));
-    Casilla10j1.add(new Casilla(16,268, 236));
-    Casilla10j1.add(new Casilla(17,304, 271));
-    Casilla10j1.add(new Casilla(18,335, 303));
-    Casilla10j1.add(new Casilla(19,365, 335));
-    Casilla10j1.add(new Casilla(20,396, 365));
-    Casilla10j1.add(new Casilla(21,432, 392));
-    
-    Casilla10j1.add(new Casilla(22,398, 419));
-    Casilla10j1.add(new Casilla(23,366, 451));
-    Casilla10j1.add(new Casilla(24,336, 480));
-    Casilla10j1.add(new Casilla(25,303, 514));
-    Casilla10j1.add(new Casilla(26,296, 547));
-    Casilla10j1.add(new Casilla(27,302, 583));
-    Casilla10j1.add(new Casilla(28,340, 544));
-    Casilla10j1.add(new Casilla(29,372, 512));
-    Casilla10j1.add(new Casilla(30,401, 482));
-    Casilla10j1.add(new Casilla(31,432, 449));
-    Casilla10j1.add(new Casilla(32,467, 421));
-    
-    Casilla10j1.add(new Casilla(33,498, 454));
-    return Casilla10j1;
+
+    public List inicializarCasilla10j1() {
+        Casilla10j1.add(new Casilla(0, 533, 360));
+        Casilla10j1.add(new Casilla(1, 564, 331));
+        Casilla10j1.add(new Casilla(2, 597, 298));
+        Casilla10j1.add(new Casilla(3, 627, 267));
+        Casilla10j1.add(new Casilla(4, 662, 231));
+        Casilla10j1.add(new Casilla(5, 629, 200));
+        Casilla10j1.add(new Casilla(6, 590, 235));
+        Casilla10j1.add(new Casilla(7, 561, 264));
+        Casilla10j1.add(new Casilla(8, 529, 297));
+        Casilla10j1.add(new Casilla(9, 498, 328));
+        Casilla10j1.add(new Casilla(10, 466, 357));
+
+        Casilla10j1.add(new Casilla(11, 434, 329));
+        Casilla10j1.add(new Casilla(12, 400, 299));
+        Casilla10j1.add(new Casilla(13, 369, 268));
+        Casilla10j1.add(new Casilla(14, 337, 238));
+        Casilla10j1.add(new Casilla(15, 300, 204));
+        Casilla10j1.add(new Casilla(16, 268, 236));
+        Casilla10j1.add(new Casilla(17, 304, 271));
+        Casilla10j1.add(new Casilla(18, 335, 303));
+        Casilla10j1.add(new Casilla(19, 365, 335));
+        Casilla10j1.add(new Casilla(20, 396, 365));
+        Casilla10j1.add(new Casilla(21, 432, 392));
+
+        Casilla10j1.add(new Casilla(22, 398, 419));
+        Casilla10j1.add(new Casilla(23, 366, 451));
+        Casilla10j1.add(new Casilla(24, 336, 480));
+        Casilla10j1.add(new Casilla(25, 303, 514));
+        Casilla10j1.add(new Casilla(26, 296, 547));
+        Casilla10j1.add(new Casilla(27, 302, 583));
+        Casilla10j1.add(new Casilla(28, 340, 544));
+        Casilla10j1.add(new Casilla(29, 372, 512));
+        Casilla10j1.add(new Casilla(30, 401, 482));
+        Casilla10j1.add(new Casilla(31, 432, 449));
+        Casilla10j1.add(new Casilla(32, 467, 421));
+
+        Casilla10j1.add(new Casilla(33, 498, 454));
+        return Casilla10j1;
     }
-    
-    public List inicializarCaminoJugador2Tablero10(){
+
+    public List inicializarCaminoJugador2Tablero10() {
         casilla10J2.add(new Casilla(0, 434, 329));
         casilla10J2.add(new Casilla(1, 400, 299));
         casilla10J2.add(new Casilla(2, 369, 268));
@@ -74,7 +76,7 @@ public class CasillaPartida {
         casilla10J2.add(new Casilla(8, 365, 335));
         casilla10J2.add(new Casilla(9, 396, 365));
         casilla10J2.add(new Casilla(10, 432, 392));
-        
+
         casilla10J2.add(new Casilla(11, 398, 419));
         casilla10J2.add(new Casilla(12, 366, 451));
         casilla10J2.add(new Casilla(13, 336, 480));
@@ -85,7 +87,7 @@ public class CasillaPartida {
         casilla10J2.add(new Casilla(18, 372, 512));
         casilla10J2.add(new Casilla(19, 401, 482));
         casilla10J2.add(new Casilla(20, 432, 449));
-        
+
         casilla10J2.add(new Casilla(21, 467, 421));
         casilla10J2.add(new Casilla(22, 498, 454));
         casilla10J2.add(new Casilla(23, 527, 485));
@@ -96,11 +98,95 @@ public class CasillaPartida {
         casilla10J2.add(new Casilla(28, 626, 512));
         casilla10J2.add(new Casilla(29, 593, 483));
         casilla10J2.add(new Casilla(30, 563, 451));
-        
+
         casilla10J2.add(new Casilla(31, 533, 422));
         casilla10J2.add(new Casilla(32, 499, 389));
         casilla10J2.add(new Casilla(33, 533, 360));
-        
+
         return casilla10J2;
+    }
+
+    public List inicializarCaminoJugador3Tablero10() {
+        casilla10J3.add(new Casilla(0, 398, 419));
+        casilla10J3.add(new Casilla(1, 366, 451));
+        casilla10J3.add(new Casilla(2, 336, 480));
+        casilla10J3.add(new Casilla(3, 303, 514));
+        casilla10J3.add(new Casilla(4, 269, 547));
+        casilla10J3.add(new Casilla(5, 302, 583));
+        casilla10J3.add(new Casilla(6, 340, 544));
+        casilla10J3.add(new Casilla(7, 372, 512));
+        casilla10J3.add(new Casilla(8, 401, 482));
+        casilla10J3.add(new Casilla(9, 432, 449));
+        casilla10J3.add(new Casilla(10, 467, 421));
+
+        casilla10J3.add(new Casilla(11, 498, 454));
+        casilla10J3.add(new Casilla(12, 527, 485));
+        casilla10J3.add(new Casilla(13, 556, 520));
+        casilla10J3.add(new Casilla(14, 586, 550));
+        casilla10J3.add(new Casilla(15, 621, 580));
+        casilla10J3.add(new Casilla(16, 658, 542));
+        casilla10J3.add(new Casilla(17, 626, 512));
+        casilla10J3.add(new Casilla(18, 593, 483));
+        casilla10J3.add(new Casilla(19, 563, 451));
+        casilla10J3.add(new Casilla(20, 533, 422));
+
+        casilla10J3.add(new Casilla(21, 499, 389));
+        casilla10J3.add(new Casilla(22, 533, 360));
+        casilla10J3.add(new Casilla(23, 564, 331));
+        casilla10J3.add(new Casilla(24, 597, 298));
+        casilla10J3.add(new Casilla(25, 627, 267));
+        casilla10J3.add(new Casilla(26, 662, 231));
+        casilla10J3.add(new Casilla(27, 629, 200));
+        casilla10J3.add(new Casilla(28, 590, 235));
+        casilla10J3.add(new Casilla(29, 561, 264));
+        casilla10J3.add(new Casilla(30, 529, 297));
+
+        casilla10J3.add(new Casilla(31, 498, 328));
+        casilla10J3.add(new Casilla(32, 466, 357));
+        casilla10J3.add(new Casilla(33, 434, 329));
+
+        return casilla10J3;
+    }
+
+    public List inicializarCaminoJugador4() {
+        casilla10J4.add(new Casilla(0, 498, 454));
+        casilla10J4.add(new Casilla(1, 527, 485));
+        casilla10J4.add(new Casilla(2, 556, 520));
+        casilla10J4.add(new Casilla(3, 586, 550));
+        casilla10J4.add(new Casilla(4, 621, 580));
+        casilla10J4.add(new Casilla(5, 658, 542));
+        casilla10J4.add(new Casilla(6, 626, 512));
+        casilla10J4.add(new Casilla(7, 593, 483));
+        casilla10J4.add(new Casilla(8, 563, 451));
+        casilla10J4.add(new Casilla(9, 533, 422));
+        casilla10J4.add(new Casilla(10, 499, 289));
+
+        casilla10J4.add(new Casilla(11, 533, 360));
+        casilla10J4.add(new Casilla(12, 564, 331));
+        casilla10J4.add(new Casilla(13, 597, 298));
+        casilla10J4.add(new Casilla(14, 627, 267));
+        casilla10J4.add(new Casilla(15, 662, 231));
+        casilla10J4.add(new Casilla(16, 629, 200));
+        casilla10J4.add(new Casilla(17, 590, 235));
+        casilla10J4.add(new Casilla(18, 561, 264));
+        casilla10J4.add(new Casilla(19, 529, 297));
+        casilla10J4.add(new Casilla(20, 498, 328));
+
+        casilla10J4.add(new Casilla(21, 466, 357));
+        casilla10J4.add(new Casilla(22, 434, 329));
+        casilla10J4.add(new Casilla(23, 400, 299));
+        casilla10J4.add(new Casilla(24, 369, 268));
+        casilla10J4.add(new Casilla(25, 337, 238));
+        casilla10J4.add(new Casilla(26, 300, 204));
+        casilla10J4.add(new Casilla(27, 268, 236));
+        casilla10J4.add(new Casilla(28, 304, 271));
+        casilla10J4.add(new Casilla(29, 335, 303));
+        casilla10J4.add(new Casilla(30, 365, 335));
+
+        casilla10J4.add(new Casilla(31, 396, 365));
+        casilla10J4.add(new Casilla(32, 432, 392));
+        casilla10J4.add(new Casilla(33, 398, 419));
+
+        return casilla10J4;
     }
 }
