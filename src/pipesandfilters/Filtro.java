bd4fc43674bd352c2ee3699db7a1d2c607960cb5
LAPTOP-CPPRNHU9\luisg <luisgonzalocervantes@gmail.com>
2022-11-16 11:39:13 -0700

    Creación de la clase de filtros y la tubería para ellos

---
+++ src/pipesandfilters/Filtro.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pipesandfilters;
+
+/**
+ *
+ * @author Luis Gonzalo Cervantes Rivera 00000228549 Gabriel Francisco Piñuelas
+ * Ramos 00000230626 Ricardo Pacheco Urias 00000229178 Abraham Sered Gómez
+ * Martínez 00000228796
+ */
+public class Filtro {
+
+    public void filtroJugadorGanoPartida() {
+
+    }
+
+    public void filtroJugadorPerdio() {
+
+    }
+
+    public void filtroFinalizadoPartida() {
+
+    }
+
+    public void filtroJugadorAvanza10() {
+
+    }
+
+    public void filtroJugadorSeUnio() {
+
+    }
+
+    public void filtroCasillaRoja() {
+
+    }
+
+    public void filtroPartidaInicio() {
+
+    }
+
+    public void filtroJugadorAbandonoPartida() {
+
+    }
+}
